5e633f0bcb7b3cad5fa6bab154ec5a6923b3e07a
Obsidianpineapple <adamarmentsr@gmail.com>
2026-07-21 09:35:00 -0400

    eternally moving button that will not leave screen

--- Click.py
+++ Click.py
@@ -2,20 +2,29 @@ import tkinter
 r = 0
 num = 0
 t = 16
+f = False
 def seven():
     global num, t
     print('Your score is adding.')
     num = num + 1
+    print(num)
     t = int(t /2)
 
 
 def move():
-    global r, t
-    r=r+1
+    global r, t, f
+    if r == 900:
+        f = True
+    if r == 0:
+        f = False
+    if f == False:
+        r=r+1
+    if f == True:
+        r = r-1
     y.place(x = 100, y = r)
     root.after(t, move)
 root = tkinter.Tk()
-x= tkinter.Canvas(root, height = 1000, width = 500)
+x= tkinter.Canvas(root, height = 900, width = 500)
 x.pack()
 y = tkinter.Button(root, bg = 'red', height = 2, width = 20, command = seven)
 y.place(x = 100, y = r)
