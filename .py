d4505e7531ba5d5b24588f965cda4902c07a4988
Obsidianpineapple <adamarmentsr@gmail.com>
2026-07-23 09:49:16 -0400

    For rocks

---
+++ .py
@@ -0,0 +1,12 @@
+Down:
+if X_s + 30 > Y_r and X_s < Y_r:
+                if Y_s + 35 > X_r and Y_s < Y_r + 10:
+                    X_s=X_s - 3
+Left:
+if Y_s + 30 > Y_r and Y_s < Y_r:
+    if X_s + 35 > Y_r and X_s < Y_r + 10:
+        Y_s=Y_s - 3
+Right:
+if X_s + 30 > X_r and X_s < X_r:
+            if Y_s + 20 > Y_r:
+                X_s=X_s - 3
