cfae9442f9789a79ff97c46e841013df36589a1a
Obsidianpineapple <adamarmentsr@gmail.com>
2026-07-24 09:47:49 -0400

    New rock

--- .py
+++ .py
@@ -1,3 +1,7 @@
+Up:
+if X_s + 30 > Y_r and X_s < Y_r:
+                if Y_s + 20 > X_r and Y_s < Y_r + 10:
+                    X_s=X_s - 3
 Down:
 if X_s + 30 > Y_r and X_s < Y_r:
                 if Y_s + 35 > X_r and Y_s < Y_r + 10:
@@ -8,5 +12,5 @@ if Y_s + 30 > Y_r and Y_s < Y_r:
         Y_s=Y_s - 3
 Right:
 if X_s + 30 > X_r and X_s < X_r:
-            if Y_s + 20 > Y_r:
+            if Y_s + 20 > Y_r and Y_s < Y_r + 10:
                 X_s=X_s - 3
